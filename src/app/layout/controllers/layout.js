aeb7ee1c2202b899837d5f2581223cda82eeebe3
Andrew Cunliffe <andrew.c.cunliffe@gmail.com>
2015-02-25 23:30:56 +0000

    Remove debug message

--- src/app/layout/controllers/layout.js
+++ src/app/layout/controllers/layout.js
@@ -1,7 +1,6 @@
 angular.module('controller.layout', [])
     .controller('LayoutCtrl', ['$rootScope', '$location', function ($scope, $location) {
         $scope.$on('$routeChangeError', function(evt, message) {
-            console.log(message);
             $location.url('404');
         });
     }]);
